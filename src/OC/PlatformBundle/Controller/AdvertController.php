9df8c4dff85745278bc3f1e9f1a9cc161c91e70f
Julien HABERT <Julien HABERT>
2016-11-17 17:05:22 +0100

    suite du cours

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -2,19 +2,33 @@
 
 namespace OC\PlatformBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
-    {
-        $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Julien'));
-        return new Response($content);
+    public function indexAction($page){
+        if($page < 1) {
+            throw new NotFoundHttpException('Page '.$page.' inexistante ! Oups.');
+        }
+        return $this->render('OCPlatformBundle:Advert:index.html.twig');
     }
 
-    public function viewAction($id)
-    {
-        return new Response('Autre Action, autre Route, autre twig ? Non pas de twig là dessus '.$id);
+    public function addAction($id, Request $request){
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('info','Annonce enregistrée !');
+            return $this->redirectToRoute('oc_platform_view', array('id' => $id));
+        }
+
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    public function viewAction($id, Request $request){
+        $tag = $request->query->get('tag');
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id,
+            'tag' => $tag,
+        ));
     }
 }
